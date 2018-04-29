a9109cb4e997a4e3825bc124239f73a0410bbf94
Sergio <Sergio@Sergio-PC>
2018-04-29 18:11:03 -0300

    agregado del Punto07y08 - Sergio Romero

---
+++ src/java/aplicacion/modelo/dominio/Libro.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplicacion.modelo.dominio;
+
+/**
+ * 
+ * @author Sergio Romero
+ */
+public class Libro {
+    private String ISBN;
+    private String titulo;
+    private String autor;
+    private double precio;
+
+    public Libro() {
+    }
+
+    public Libro(String ISBN, String titulo, String autor, double precio) {
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }
+
+    /**
+     * @return the ISBN
+     */
+    public String getISBN() {
+        return ISBN;
+    }
+
+    /**
+     * @param ISBN the ISBN to set
+     */
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
